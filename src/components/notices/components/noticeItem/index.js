8e19b727214680d644b239079caec24ed571ee4a
anliang <liang.an@ncsi.com.cn>
2024-01-18 16:40:27 +0800

    改善通知功能

--- src/components/notices/components/noticeItem/index.js
+++ src/components/notices/components/noticeItem/index.js
@@ -3,57 +3,22 @@ import styles from'./style.module.less'
 import {IonIcon} from "@ionic/react"
 import { close } from 'ionicons/icons';
 import {connect} from 'react-redux'
-const NoticesItem =({data,index,notices,updateNotice,stopUpdateNotice}) => {
+const NoticesItem =({data,index,notices,stopUpdateNotice}) => {
     const [length]=useState(notices.filter(it=>!it.unmont).length)
     const countRef = useRef(stopUpdateNotice);
-    const timerRef = useRef(null);
+    
     useEffect(()=>{
-        // setLength(notices.filter(it=>!it.unmont).length)
-        // stopUpdateNotice && clearTimeout(timer)
         if(!stopUpdateNotice){
-            console.log('!!!!!!!!',length)
-            
-            // timerRef.current = setTimeout(() => {
-                
-            //     console.log('data------------',data)
-                updateNotice(false,data)
-            //     // setToUnmont(true)
-            // }, 3000);
+            // updateNotice(false,data)
         }else{
-            // console.log('timer',timerRef.current)
-            // clearTimeout(timerRef.current)
             return
         }
         return ()=>{
         }
     },[])
-    // useEffect(()=>{
-    //     console.log("stopUpdateNotice",stopUpdateNotice)
-    //     if(length){
-    //         countRef.current = stopUpdateNotice;
-    //         let unmontLength = notices.filter(it=>it.unmont).length
-    //         // if(!stopUpdateNotice){
-    //         //     console.log('!!!!!!!!',countRef.current,length)
-                
-    //         //     timerRef.current = setTimeout(() => {
-    //         //         console.log(',,,,,,,,,,,,,',countRef.current,length)
-                    
-    //         //         if(countRef.current)  {
-                        
-    //         //             return
-    //         //         };
-    //         //         console.log('data------------',data)
-    //         //         updateNotice(data)
-    //         //         // setToUnmont(true)
-    //         //     }, (stopUpdateNotice ? (length-unmontLength) : length) * 6000);
-    //         console.log('???????',index)
-            
-    //     }
-    //     // stopUpdateNotice && clearTimeout(timer)
-    // },[stopUpdateNotice])
     return (
-        !data.unmont && 
-        <div className={[styles.box+' '+"box pa24 maB12 bg3 borderR12"]}>
+        
+        <div className={[styles.box+' '+`pa24 maB12 bg3 borderR12 ${(data.unmont ? styles.toUnmont:"")}`]}>
             {
                 !data.unmont && 
                 <>
--- src/components/notices/index.js
+++ src/components/notices/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react"
+import React, {useEffect, useState, useRef} from "react"
 import NoticesItem from "./components/noticeItem";
 import styles from './style.module.less'
 import { connect } from 'react-redux';
@@ -7,8 +7,10 @@ import {IonIcon} from "@ionic/react";
 import { close } from 'ionicons/icons';
 import socket from "../../http/socke"
 const NoticesBox =() => {
-    const [stopUpdateNotice,setStopUpdateNotice]=useState(false)
+    // const [stopUpdateNotice,setStopUpdateNotice]=useState(false)
     const [notices,setNotices]=useState([])
+    const timerRef = useRef(null);
+    const stopUpdateNotice = useRef(false);
     useEffect(()=>{
         socket.on('message', (data) => {
             console.log('接收到服务器消息:', data);
@@ -17,19 +19,32 @@ const NoticesBox =() => {
             })
         });
         return ()=>{
-            // clearInterval(timer)
+            clearInterval(timerRef.current)
         }
     },[])
+    useEffect(()=>{
+        if(notices.length>0){
+            let falseArr = notices.filter(item=>!item.unmont)
+            falseArr && handleControllUpdate(stopUpdateNotice.current,falseArr[0])
+        }
+    },[notices])
     const handleControllUpdate = (controllUpdate,item) => {
-        // controllUpdateNotice(!stopUpdateNotice)
-        setStopUpdateNotice(controllUpdate)
-        setTimeout(() => {
-            // setToUnmont(true)
-            handleUpdateNotice(item ? item : notices[0])
-        }, 3000);
+        stopUpdateNotice.current=controllUpdate;
+        if(!stopUpdateNotice.current){
+            timerRef.current=setTimeout(() => {
+                if(stopUpdateNotice.current){
+                    clearInterval(timerRef.current);
+                    return;
+                }
+                handleUpdateNotice(item ? item : notices[0])
+            }, 5000);
+        }else{
+            console.log("timerRef.current",timerRef.current)
+            clearInterval(timerRef.current)
+        }
+        
     }
     const handleUpdateNotice = (data) => {
-        
           setNotices(pre=>{
             let newNotices = pre.map(item=>{
                 if(item.id==data.id){
@@ -38,7 +53,6 @@ const NoticesBox =() => {
                   return item
                 }
               })
-            console.log("vvvvvvvvvvvvvv",pre,data)
             return newNotices
         })
     }
@@ -48,9 +62,9 @@ const NoticesBox =() => {
     return (
         notices.length>0 && 
         <div className={styles.box} onMouseEnter={()=>handleControllUpdate(true)} onMouseLeave={()=>handleControllUpdate(false)}>
-            <IonIcon className={styles.clear_btn+' '+"maB12 bg1 pa6 borderR50 "} icon={close} size="18px" onClick={handleClearNotices}></IonIcon>
-            <div>
-                {notices.map((item,index)=><NoticesItem key={item.id} index={index} stopUpdateNotice={stopUpdateNotice} notices={notices} updateNotice={handleControllUpdate} data={item}/>)}
+            <IonIcon className={styles.clear_btn+' '+"maB12 bg1 pa6 borderR50"} icon={close} size="18px" onClick={handleClearNotices}></IonIcon>
+            <div className={["scrollbarBox"]} >
+                {notices.map((item,index)=><NoticesItem key={item.id} index={index} stopUpdateNotice={stopUpdateNotice} notices={notices}  data={item}/>)}
             </div>
         </div>
     )
