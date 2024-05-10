90573dd45ef51ec035bfcb0ad021a196c666cb00
anln <anli_ang@yeah.net>
2024-05-11 04:37:27 +0800

    add SpeedInsights

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import App from './app'
 import { Provider } from 'react-redux';
 import store from './store'
 import favicon from './assets/favicon.ico';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 // const nodeServer = require('./server');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <App />
+      <SpeedInsights />
     </Provider>
   );
