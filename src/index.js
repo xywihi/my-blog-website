d0ee0f8c26c322b29d41ce0b10beac9f44a57e0c
anln <anli_ang@yeah.net>
2024-05-11 04:47:56 +0800

    fix error

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './app'
 import { Provider } from 'react-redux';
 import store from './store'
 import favicon from './assets/favicon.ico';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 // const nodeServer = require('./server');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
