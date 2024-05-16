aba1a26c992a5d68453cca7030c32bee0f5450e5
anln <anli_ang@yeah.net>
2024-05-16 08:40:11 +0800

    change styles

--- src/pages/cards/components/OpenAiBox/index.js
+++ src/pages/cards/components/OpenAiBox/index.js
@@ -4,16 +4,12 @@ import styles from "./index.module.less";
 import { IonIcon } from "@ionic/react";
 import { Loader } from "@/components/animaIcons";
 import robot from "@/assets/images/robot.svg";
-import {speak} from "@/util"
+import { speak } from "@/util";
 export default function OpenAiBox() {
   const textareaRef = useRef(null);
   const [loading, setLoading] = useState(false);
   const [aiAnswer, setAiAnswer] = useState("");
-  const openai = new OpenAI({
-    baseURL: "https://api.fe8.cn/v1",
-    apiKey: "sk-dCslKhodCvB8wVJAdIrAV9lHze8QhbfJmFLBWlwFGMVHDi8I",
-    dangerouslyAllowBrowser: true,
-  });
+
   const [inputValue, setInputValue] = useState("");
   useEffect(() => {
     textareaRef.current.addEventListener("input", function () {
@@ -33,17 +29,38 @@ export default function OpenAiBox() {
   }, []);
   const handleOpenAI = async (question) => {
     setLoading(true);
-    const completion = await openai.chat.completions.create({
-      messages: [{ role: "system", content: question }],
-      model: "gpt-3.5-turbo",
-    });
-    setLoading(false);
-    let answers = completion.choices[0].message.content;
-    setAiAnswer(old=>{
-    // 文字转语音
-      speak(answers,7);
-      return answers;
-    });
+    try {
+      const openai = new OpenAI({
+        baseURL: "https://api.fe8.cn/v1",
+        // apiKey: "sk-dCslKhodCvB8wVJAdIrAV9lHze8QhbfJmFLBWlwFGMVHDi8I",
+        dangerouslyAllowBrowser: true,
+      });
+      const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content: question }],
+        model: "gpt-3.5-turbo",
+      });
+      setLoading(false);
+      let answers = completion.choices[0].message.content;
+      setAiAnswer((old) => {
+        // 文字转语音
+        speak(answers, 7);
+        return answers;
+      });
+    } catch (error) {
+      let timer = setTimeout(() => {
+        setLoading(false);
+        setAiAnswer((old) => {
+          // 文字转语音
+          speak(
+            "Hello，现在我是你的的私人小助理大山,随时解决你的问题。",
+            7
+          );
+
+          return "Hello，现在我是你的的私人小助理大山,随时解决你的问题。";
+        });
+        clearTimeout(timer);
+      },1500);
+    }
   };
   // 我是Anln，现在你是我的私人小助理爱丽,每天早上9:00和晚上18:00需要向我打招呼和报时和解决我的问题。
   return (
@@ -60,7 +77,10 @@ export default function OpenAiBox() {
               value={inputValue}
             />
           </div>
-          <div className={`textCenter font46 ${styles.robotBox}`} onClick={()=>aiAnswer != "" && speak(aiAnswer)}>
+          <div
+            className={`textCenter font46 ${styles.robotBox}`}
+            onClick={() => aiAnswer != "" && speak(aiAnswer,7)}
+          >
             {loading ? (
               <Loader />
             ) : (
